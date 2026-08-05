18abc5659133b5dc7d4bfdf5d261fac116f6690a
SJopez <mr.sergiolooolll@gmail.com>
2026-08-05 14:22:35 -0400

    menu style

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -33,9 +33,22 @@ function InputField(props: Input) {
     let currInput = useRef<HTMLInputElement>(null);
 
     return (
-        <div id='inputContainer'>
-            <input ref={currInput} id='inputField' type="text" placeholder='Enter your username...' defaultValue={"Tourist"}></input>
-            <button id='inputButton' onClick={() => props.onchange(currInput.current?.value!)}>Submit</button>
+        <div id='inputWrapper'>
+            <div id='inputContainer'>
+                <input
+                    ref={currInput}
+                    id='inputField'
+                    type="text"
+                    placeholder='Enter your username...'
+                    defaultValue={"Tourist"}
+                />
+                <button id='inputButton' onClick={() => props.onchange(currInput.current?.value!)}>
+                    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="11" cy="11" r="7" stroke="currentColor" strokeWidth="2" />
+                        <line x1="16.5" y1="16.5" x2="21" y2="21" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
+                    </svg>
+                </button>
+            </div>
         </div>
     )
 }
@@ -51,6 +64,9 @@ export default function Menu(props: MenuOptions) {
 
     return (
         <div id='menu' ref={menu}>
+            <h1 id='menuTitle'>
+                Yu-Gi-Oh! <span>Codeforces Stats</span>
+            </h1>
             <InputField onchange={setUsername}></InputField>
             <Card 
                 username={username}
