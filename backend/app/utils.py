981d6fe2d9b69f8e7b01151670be71bab92ee73d
10Y6 <alexmanuel1900@gmail.com>
2026-08-06 17:52:20 -0400

    refactor(backend): separate services and utils functions from main

---
+++ backend/app/utils.py
@@ -0,0 +1,102 @@
+from collections import defaultdict
+
+async def unique_solved_problems(response_2):
+    problems = dict()
+    for sub in response_2:
+        if sub.get('verdict') == 'OK':
+            contesId = sub.get('contestId')
+            problem_index = sub.get('problem').get('index')
+            
+            prog_lang = sub.get('programmingLanguage')
+            tags = sub.get('problem').get('tags')
+            rating = sub.get('problem').get('rating')
+
+            #null rating == unrated contest
+
+            problems.update({
+                f'{contesId}_{problem_index}' : {
+                    'prog_lang' : prog_lang,
+                    'tags' : tags,
+                    'rating' : rating
+                } 
+            })
+
+    return problems
+
+def normalize_lang(lang: str) -> str:
+    if not lang:
+        return "Otros"
+    
+    lang_lower = lang.lower()
+    
+    if "c++" in lang_lower or "g++" in lang_lower:
+        return "C++"
+    if "c#" in lang_lower or "mono c#" in lang_lower:
+        return "C#"
+    if "c11" in lang_lower or "gnu c" in lang_lower or lang_lower == "c":
+        return "C"
+    if "python" in lang_lower or "pypy" in lang_lower:
+        return "Python"
+    if "java" in lang_lower:
+        return "Java"
+    if "kotlin" in lang_lower:
+        return "Kotlin"
+    if "rust" in lang_lower:
+        return "Rust"
+    if "pascal" in lang_lower or "fpc" in lang_lower:
+        return "Pascal"
+    if "f#" in lang_lower:
+        return "F#"
+    
+    mapping = {
+        "go": "Go",
+        "haskell": "Haskell",
+        "javascript": "JavaScript",
+        "node.js": "JavaScript",
+        "scala": "Scala",
+        "ruby": "Ruby",
+        "php": "PHP",
+        "perl": "Perl",
+        "ocaml": "OCaml",
+        "delphi": "Delphi",
+        "d": "D",
+        "tcl": "Tcl",
+        "io": "Io",
+        "pike": "Pike",
+        "befunge": "Befunge",
+        "cobol": "Cobol",
+        "factor": "Factor",
+        "roco": "Roco",
+        "ada": "Ada",
+        "false": "FALSE",
+        "picat": "Picat",
+        "j": "J"
+    }
+    
+    return mapping.get(lang_lower, lang.split()[0] if lang else "Otros")
+
+async def most_used_lang(problems):
+    langs = defaultdict(int)
+
+    for problem in problems.values():
+        raw_lang = problem.get('prog_lang')
+        if raw_lang:
+            norm_lang = normalize_lang(raw_lang)
+            langs[norm_lang] += 1
+    
+    if not langs:
+        return None
+
+    best_lang = max(langs.items(), key=lambda x: x[1])
+    return best_lang[0]
+
+
+async def get_problems_tags(problems : dict):
+    tag = defaultdict(int)
+
+    for problem in problems.values():
+        for tag_ in problem.get('tags'):
+            tag[tag_] += 1
+
+    tag = sorted(tag, key=lambda x: tag.get(x), reverse=True)
+    return tag
