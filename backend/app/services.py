dd92e410db23fd0e6ed5bd1a3dcf093c42d2f164
10Y6 <alexmanuel1900@gmail.com>
2026-08-09 17:48:43 -0400

    fix(backend): now rank is returned in lowercase

--- backend/app/services.py
+++ backend/app/services.py
@@ -81,7 +81,7 @@ async def process_null_rated(user_info : dict = None):
     try :
         rank = soup.select_one('div.user-rank span').text[:-1]
         max_rank : str = li.select_one('span.smaller').select('span')[0].text
-        max_rank = max_rank.capitalize()[:-2]
+        max_rank = max_rank[:-2]
 
         rating = span[0].text
         max_rating = li.select_one('span.smaller').select('span')[1].text
@@ -92,8 +92,8 @@ async def process_null_rated(user_info : dict = None):
         max_rating = 0
 
     user_info.update({
-        'rank' : rank,
-        'max_rank' : max_rank,
+        'rank' : rank.lower(),
+        'max_rank' : max_rank.lower(),
         'rating' : rating,
         'max_rating' : max_rating
     })
